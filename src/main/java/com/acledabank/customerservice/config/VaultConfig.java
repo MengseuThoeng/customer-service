ac1de64d90b99dac1e23bbc1c216e4e21ae813b6
MengseuThoeng <mengseu2004@gmail.com>
2025-11-26 11:55:18 +0700

    Testing Vault 3

---
+++ src/main/java/com/acledabank/customerservice/config/VaultConfig.java
@@ -0,0 +1,28 @@
+package com.acledabank.customerservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.cloud.config")
+public class VaultConfig {
+    
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
